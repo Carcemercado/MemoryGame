4f1615cc3040d610d554d00c9a0c5a7550dee08f
Carlos Arce <arce.code@gmail.com>
2019-05-30 20:56:46 -0400

    Updated files via upload.
    Added functions to track player moves, reset game button.
    Will continue to work to finish game.

--- fend-project-memory-game-master/js/app.js
+++ fend-project-memory-game-master/js/app.js
@@ -5,12 +5,10 @@
 const cardIcons = ["fa fa-diamond", "fa fa-paper-plane-o", "fa fa-anchor", "fa fa-bolt", "fa fa-cube", 
                     "fa fa-anchor", "fa fa-leaf", "fa fa-bicycle", "fa fa-diamond", "fa fa-bomb", "fa fa-leaf",
                     "fa fa-bomb", "fa fa-bolt", "fa fa-bicycle", "fa fa-paper-plane-o", "fa fa-cube"];
-
 //Assigns .deck class to cardsContainer.
 const cardsContainer = document.querySelector(".deck");
 let openCards = [];//Open array to hold clicked open cards.
 let matchedCards = [];//Open array to hold matched cards.
-
 /*
 * Begins the game
 */
@@ -25,13 +23,11 @@ function beginGame(){
         clickCard(card);
     }
 }
-
     //Click event
     function clickCard(card){
     //Create click event listener for cards.
     card.addEventListener("click", function (){
         console.log(card.innerHTML);//Test code, see if console displays inner HTML on click.
-        
         const currentCard = this;
         const previousCard = openCards[0];//Code to compare 2 opened cards. checks first element of array [0] - arrays begin at 0 :)
 
@@ -42,7 +38,6 @@ function beginGame(){
             openCards.push(this);//Pushes selected cards to openCards variable.
         //Compare function.
         compareCards(currentCard, previousCard);
-
         } else{
         //Code for when no cards are selected.
             currentCard.classList.add("open", "show", "disabled");
@@ -50,7 +45,6 @@ function beginGame(){
         }
     });
 }
-
 /*
 * Compare function to compare the cards. Comparison of 2 cards.
 */
@@ -58,7 +52,6 @@ function compareCards(currentCard, previousCard){
     //Compares current card and previous card.
     if (currentCard.innerHTML === previousCard.innerHTML) {
         console.log("Cards matched!");//Test game logic for a match.
-
         /*
         Adds css class .match to all matched cards.
         Add .match css class to current card.
@@ -69,12 +62,10 @@ function compareCards(currentCard, previousCard){
         matchedCards.push(currentCard, previousCard);
         //Resets openCards value after cards are matched.
         openCards = [];
-
         //Check if the game is over.
         gameOver();
     } else {
         console.log("Card mismatch! Try again!");//Test game logic for mismatch.
-
         ////Delays css action by waiting 800 milliseconds.
         setTimeout(function () {
             //Remove .open .show css class //Remove .open .show css class //Removed .disabled css
@@ -84,17 +75,38 @@ function compareCards(currentCard, previousCard){
             openCards = [];
         }, 800);
     }
+    //Add moves
+    addMoves();
 }
-
 function gameOver(){
     if (matchedCards.length === cardIcons.length){
         alert("Game Over, You Won!")
     }
 }
-
 //***/Begin the game when game is loaded./***///
 beginGame();
-
+/*
+Restart Game Button
+*/
+const restartButton = document.querySelector(".restart");
+restartButton.addEventListener("click", function(){
+    //Delete all cards on current game.
+    cardsContainer.innerHTML = "";
+    
+    //Call beginGame() function to create new cards for new game.
+    beginGame();
+    //Reset all related game variables, like the matched cards.
+    
+})
+/*
+Add moves to game, count player clicks.
+*/
+const movesContainer = document.querySelector(".moves");
+let playerMoves = 0;
+function addMoves(){
+    playerMoves++;
+    movesContainer.innerHTML = playerMoves;
+}
 // Shuffle function from http://stackoverflow.com/a/2450976
 function shuffle(array) {
     var currentIndex = array.length, temporaryValue, randomIndex;
@@ -108,15 +120,3 @@ function shuffle(array) {
     }
     return array;
 }
-
-
-/*
- * set up the event listener for a card. If a card is clicked:
- *  - display the card's symbol (put this functionality in another function that you call from this one)
- *  - add the card to a *list* of "open" cards (put this functionality in another function that you call from this one)
- *  - if the list already has another card, check to see if the two cards match
- *    + if the cards do match, lock the cards in the open position (put this functionality in another function that you call from this one)
- *    + if the cards do not match, remove the cards from the list and hide the card's symbol (put this functionality in another function that you call from this one)
- *    + increment the move counter and display it on the page (put this functionality in another function that you call from this one)
- *    + if all cards have matched, display a message with the final score (put this functionality in another function that you call from this one)
- */
